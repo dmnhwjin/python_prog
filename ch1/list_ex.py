2d9c9e0aa105fd2b8294fc6fe9f585be220d35c6
dmnhwjin <41839920@qq.com>
2018-09-30 19:17:04 +0800

    commit list_ext

--- ch1/list_ex.py
+++ ch1/list_ex.py
@@ -1,1 +1,39 @@
 """This is a list example"""
+
+
+bob = ['Bob Smith', 42, 30000, 'software']
+sue = ['Sue Jones', 45, 40000, 'hardware']
+
+bob[0], sue[2]
+
+
+bob[0].split()[-1]
+
+sue[2] *= 1.25
+
+sue
+
+
+people = [bob, sue]
+for person in people:
+    print(person)
+    
+people[1][0]
+
+for person in people:
+    print(person[0].split()[-1])
+    person[2] *= 1.20
+    
+for person in people: print(person[2])
+    
+    
+pays = [person[2] for person in people]
+
+pays
+
+pays = map((lambda x: x[2]), people)
+
+list(pays)
+
+sum(person[2] for person in people)
+
